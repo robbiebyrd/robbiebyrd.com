843dd58ea0216b5174300d71fc2aea19d338be87
Robbie Byrd <me@robbiebyrd.com>
2022-07-10 21:33:55 -0500

    Move data to its own file

---
+++ js/data.js
@@ -0,0 +1,12 @@
+var data = {
+"http://robbiebyrd.com/homepage":
+`# Blog
+## this is my blog
+this is my blog yo`,
+"http://robbiebyrd.com/archive":
+`# Archive
+this is my archive yo`,
+"http://www.aol.com":
+`# AOL
+# beeep beeep boop boooooopppppp`,
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-data = {
-"http://robbiebyrd.com/homepage":
-`# Blog
-## this is my blog
-this is my blog yo`,
-"http://robbiebyrd.com/archive":
-`# Archive
-this is my archive yo`,
-"http://www.aol.com":
-`# AOL
-# beeep beeep boop boooooopppppp`,
-}
-
 const history = []
 var position = 0
 jQuery(function () {
