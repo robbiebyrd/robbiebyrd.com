9e5d9feb526088cef6c5c77ad5ddafb1c72f21d7
Robbie Byrd <me@robbiebyrd.com>
2022-07-26 19:38:46 -0500

    Add screensaver, some additional desktop icon functionality

--- js/main.js
+++ js/main.js
@@ -1,63 +1,90 @@
-const history = []
-var position = 0
-jQuery(function () {
-	history.push("http://robbiebyrd.com/homepage")
-	updateBrowser("http://robbiebyrd.com/homepage")
-	jQuery(".favorite").on("click touch", function () {
-		clickURL(jQuery(this).data("url"))
+const history = [];
+var position = 0;
+var idleTime = 0;
+// Increment the idle time counter every minute.
+
+jQuery(function() {
+	history.push('http://robbiebyrd.com/homepage');
+	updateBrowser('http://robbiebyrd.com/homepage');
+	jQuery('.favorite').on('click touch', function() {
+		clickURL(jQuery(this).data('url'));
 	});
-	jQuery(".forward_button").on("click touch", function () {
-		move(1)
+	jQuery('.forward_button').on('click touch', function() {
+		move(1);
 	});
-	jQuery(".back_button").on("click touch", function () {
-		move(-1)
+	jQuery('.back_button').on('click touch', function() {
+		move(-1);
 	});
-	jQuery("#resetIcons").on("click touch", function () {
-		resetDesktopIcons()
+	jQuery('#resetIcons').on('click touch', function() {
+		resetDesktopIcons();
 	});
-	jQuery("#sortIcons").on("click touch", function () {
-		sortDesktopIcons()
+	jQuery('#sortIcons').on('click touch', function() {
+		sortDesktopIcons();
+	});
+
+	var idleInterval = setInterval(timerIncrement, 10000); // 1 minute
+
+	// Zero the idle timer on mouse movement.
+	$(this).mousemove(function(e) {
+		idleTime = 0;
+		$('.screensaver').addClass('hidden');
+	});
+	$(this).keypress(function(e) {
+		idleTime = 0;
+		$('.screensaver').addClass('hidden');
 	});
 });
 
 function clickURL(url) {
-	if(url != history[position]){
+	if (url != history[position]) {
 		position += 1;
 		location.value = url;
-		history.push(url)
-		updateBrowser(url)
+		history.push(url);
+		updateBrowser(url);
 	}
 }
 
 function updateBrowser(url) {
-	var display = document.querySelector("#internet .inner .inner-content");
-	var location = document.querySelector(".location_input input")
-	var location_status = document.querySelector(".netscape_footer label")
+	var display = document.querySelector('#internet .inner .inner-content');
+	var location = document.querySelector('.location_input input');
+	var location_status = document.querySelector('.netscape_footer label');
 	display.innerHTML = markdown(data[url]);
 	location.value = url;
 	location_status.innerHTML = url;
 }
 
 function move(p) {
-	if((position + p) >= 0 && (position + p) < history.length) {
-		position = position + p
-		updateBrowser(history[position])
+	if (position + p >= 0 && position + p < history.length) {
+		position = position + p;
+		updateBrowser(history[position]);
 	}
 }
 
 function resetDesktopIcons() {
-	jQuery(".icon").removeAttr("style")
+	jQuery('.icon').removeAttr('style');
 }
 
 function sortDesktopIcons() {
 	var mylist = $('#icon-holder');
 	var listitems = mylist.children('div').get();
 	listitems.sort(function(a, b) {
-		var compA = $(a).attr('id').toUpperCase();
-		var compB = $(b).attr('id').toUpperCase();
-		return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-	})
+		var compA = $(a)
+			.attr('id')
+			.toUpperCase();
+		var compB = $(b)
+			.attr('id')
+			.toUpperCase();
+		return compA < compB ? -1 : compA > compB ? 1 : 0;
+	});
 	$.each(listitems, function(idx, itm) {
 		mylist.append(itm);
 	});
+}
+
+function timerIncrement() {
+	idleTime = idleTime + 1;
+	if (idleTime >= 1) {
+		// 20 minutes
+		$('.screensaver').removeClass('hidden');
+	}
 }
